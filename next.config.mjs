d3ab543eb45d07423b48eca52dc2c370cfd521de
TomasLonnblad <tomaslonnblad@gmail.com>
2026-01-20 10:04:09 +0100

    x

---
+++ next.config.mjs
@@ -0,0 +1,28 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+
+images:{domains: ['images.unsplash.com', 'https://tomaslonnblad.github.io/2.jpg']},
+
+
+  reactCompiler: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
+  async redirects() {
+    return [
+      {
+        source: "/dashboard",
+        destination: "/dashboard/default",
+        permanent: false,
+
+
+
+    
+      },
+    ];
+  },
+};
+
+
+
+export default nextConfig;
---
+++ src/app/(main)/dashboard/carousel/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import * as React from "react"
+import { Card, CardContent } from "@/components/ui/card"
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
+export default function CarouselDemo() {
+  return (
+    <Carousel className="w-full max-w-xs">
+      <CarouselContent>
+        {Array.from({ length: 5 }).map((_, index) => (
+          <CarouselItem key={index}>
+            <div className="p-1">
+<img alt="sfsda" src="https://tomaslonnblad.github.io/2.jpg" width={300} height={200} />
+
+            </div>
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  )
+}
